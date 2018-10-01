2eb45de29717e20c1003f13a60d6acb9e002363f
ddddyyyy <2115601599@qq.com>
2018-10-01 23:09:10 +0800

    * AreaCalculate.csproj: ,..,
    * Program.cs:
    * AssemblyInfo.cs: ,.,.

---
+++ AreaCalculate/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+
+namespace AreaCalculate
+{
+    class DataMaker
+    {
+        public static ArrayList Making(int seed,int count)
+        {
+            Random random = new Random(seed);
+            ArrayList list = new ArrayList();
+            while(--count>0)
+            {
+                list.Add(random.NextDouble());
+            }
+            return list;
+        }
+    }
+
+    interface IShape
+    {
+        float Arae();
+    }
+
+    class Triangle : IShape
+    {
+        readonly float height;
+        readonly float width;
+
+        public Triangle(float height,float width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public float Arae()
+        {
+            return height * width / 2;
+        }
+    }
+
+    class UShape
+    {
+        ArrayList XList;
+        ArrayList YList;
+        public UShape(int[] list){
+            XList = new ArrayList(list);
+            
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}
